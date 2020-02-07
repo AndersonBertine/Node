168429f9dd9b40b58bba022996382088d73b9cb8
Guilherme Fontes <guilhermefamorim2020@gmail.com>
2020-02-07 10:18:25 -0400

    Funções em Variaveis

---
+++ funcoes/Function.js
@@ -0,0 +1,6 @@
+const n1 = function somar(x,y){
+    return x + y
+}
+
+
+console.log(n1(5,2))
---
+++ funcoes/varFunction.js
@@ -0,0 +1,16 @@
+const num1 = 10
+
+var numero = (x) => {
+    return x + x
+}
+
+
+var divide = (x,y) => {
+    if (x > y)
+        return x / y
+    else
+        return y / x   
+}
+
+console.log(numero(10))
+console.log(divide(8,12))
